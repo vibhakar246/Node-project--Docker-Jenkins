44670e83460a33ef1eeecb5a52822c45fc80d2b8
Vibhakar <vibhakar246@gmail.com>
2026-02-12 04:00:27 +0000

    Node project with Docker + Jenkins

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end("Node CI/CD running");
+});
+
+server.listen(3000, () =>
+  console.log("Server running on port 3000")
+);
